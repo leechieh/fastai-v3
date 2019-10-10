2593d31fc4fdf24e175e639193215651c2d21954
Chieh Lee <lee.chieh@gmail.com>
2019-10-10 11:30:26 -0700

    Update server.py
    Update the export file url. Update classes to suit for dogs,

--- app/server.py
+++ app/server.py
@@ -9,10 +9,22 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'https://www.dropbox.com/s/6bgq8t6yextloqp/export.pkl?raw=1'
+export_file_url = 'https://www.googleapis.com/drive/v3/files/1-HilkIWGeE6kBmjzFxg98gCuHqY6DgFs?alt=media&key=AIzaSyA7NSst4uYg6v_yjjvc8oMIz4zqDClCAYI'
 export_file_name = 'export.pkl'
 
-classes = ['black', 'grizzly', 'teddys']
+classes = ['Affenpinscher', 'Afghan Hound', 'African Hunting Dog', 'Airedale', 'American Staffordshire Terrier', 'Appenzeller', 'Basenji', 'Basset', 'Beagle', 'Bedlington Terrier',
+			'Bernese Mountain Dog', 'Black-and-tan Coonhound', 'Blenheim Spaniel', 'Bloodhound', 'Bluetick', 'Border Collie', 'Border Terrier', 'Borzoi', 'Boston Bull', 
+			'Bouvier des Flandres', 'Boxer', 'Brabancon Griffon', 'Briard', 'Brittany Spaniel', 'Bull Mastiff', 'Cairn Terrier', 'Cardigan', 'Chesapeake Bay retriever', 
+			'Chihuahua', 'Chow', 'Clumber', 'Cocker Spaniel', 'Collie', 'Curly-coated Retriever', 'Dandie Dinmont', 'Dhole', 'Dingo', 'Doberman', 'English foxhound', 'English Setter',
+			'English Springer', 'EntleBucher', 'Eskimo Dog', 'Flat-coated Retriever', 'French Bulldog', 'German Shepherd', 'German Short-haired Pointer', 'Giant Schnauzer',
+			'Golden Retriever', 'Gordon Setter', 'Great Dane', 'Great Pyrenees', 'Greater Swiss Mountain Dog', 'Groenendael', 'Ibizan Hound', 'Irish Setter', 'Irish Terrier',
+			'Irish Water Spaniel', 'Irish Wolfhound', 'Italian Greyhound', 'Japanese Spaniel', 'Keeshond', 'Kelpie', 'Kerry Blue Terrier', 'Komondor', 'Kuvasz', 'Labrador Retriever',
+			'Lakeland Terrier', 'Leonberg', 'Lhasa', 'Malamute', 'Malinois', 'Maltese Dog', 'Mexican Hairless', 'Miniature Pinscher', 'Miniature Poodle', 'Miniature Schnauzer',
+			'Newfoundland', 'Norfolk Terrier', 'Norwegian Elkhound', 'Norwich Terrier', 'Old English Sheepdog', 'Otterhound', 'Papillon', 'Pekinese', 'Pembroke', 'Pomeranian', 'Pug',
+			'Redbone', 'Rhodesian Ridgeback', 'Rottweiler', 'Saint Bernard', 'Saluki', 'Samoyed', 'Schipperke', 'Scotch Terrier', 'Scottish Deerhound', 'Sealyham Terrier',
+			'Shetland Sheepdog', 'Shih-Tzu', 'Siberian Husky', 'Silky Terrier', 'Soft-coated Wheaten Terrier', 'Staffordshire Bullterrier', 'Standard Poodle', 'Standard Schnauzer',
+			'Sussex Spaniel', 'Tibetan Mastiff', 'Tibetan Terrier', 'Toy Poodle', 'Toy Terrier', 'Vizsla', 'Walker Hound', 'Weimaraner', 'Welsh Springer Spaniel', 
+			'West Highland White_terrier', 'Whippet', 'Wire-haired Fox Terrier', 'Yorkshire Terrier']
 path = Path(__file__).parent
 
 app = Starlette()
